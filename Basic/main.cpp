cfd0ec9d1f043d4bb19d09b5508b46d988d34865
Vijay Pakhale <vijay.pakhale100@gmail.com>
2024-03-03 19:54:02 +0530

    cmake learning1

---
+++ Basic/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "mylib.h"
+using namespace std;
+int main()
+{
+    print_hello_world();
+    return 0;
+}
---
+++ With_Subdirs/lib/my_lib1/mylib.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "mylib.h"
+using namespace std;
+
+void print_hello_world()
+{
+    std::cout << "hello world..." << std::endl;
+}
